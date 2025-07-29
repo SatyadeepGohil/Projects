063629666a13d5d592df2237d57d672d0523ab26
SatyadeepGohil <gohilsatyadeep7@gmail.com>
2025-07-29 17:19:53 +0530

    wip: Basic file logic
    - Not fully complete and needs to fix bugs

--- js-projects/JS-notes/main.js
+++ js-projects/JS-notes/main.js
@@ -1,15 +1,99 @@
-const sidebar = document.getElementById('sidebar');
-const sidebarToggleBtn = document.getElementById('sidebar-toggle-btn');
-let isToggle = false;
-let isPreveiwVisible = false;
-
 const editor = document.getElementById('editor');
 const preview = document.getElementById('preview');
+
+const sidebar = document.getElementById('sidebar');
+const sidebarToggleBtn = document.getElementById('sidebar-toggle-btn');
+
+const fileBtn = document.getElementById('file');
+const folderBtn = document.getElementById('folder');
+
+let isToggle = false;
+let isPreviewVisible = false;
+
+let currentFile = null;
+let currentFolder = null;
+
+let fileData;
+const localData = localStorage.getItem('notes-data');
+
+if (localData) {
+    fileData = JSON.parse(localData);
+} else {
+    fileData = { mynotes: {} }
+    localStorage.setItem('notes-data', JSON.stringify(fileData));
+}
+
 
 editor.addEventListener('input', () => {
     const raw = editor.value;
     preview.innerHTML = marked.parse(raw);
+    contentUpdation(raw);
 })
+
+function renderSidebar() {
+    sidebar.innerHTML = '';
+
+    Object.keys(fileData).forEach(folder => {
+        const folderButton = document.createElement('button');
+        folderButton.textContent = folder;
+        folderButton.onclick = () => {
+            currentFolder = folder;
+            renderSidebar();
+            renderFiles(folder);
+        };
+        sidebar.appendChild(folderButton);
+    });
+
+    const controls = document.createElement('div');
+    controls.appendChild(fileBtn);
+    controls.appendChild(folderBtn);
+    sidebar.appendChild(controls)
+}
+
+function renderFiles (folder) {
+    const files = fileData[folder];
+
+    Object.keys(files).forEach(file => {
+        const fileButton = document.createElement('button');
+        fileButton.textContent = file;
+        fileButton.addEventListener('click', () => {
+            currentFile = file;
+            editor.value = fileData[folder][file];
+            preview.innerHTML = marked.parse(editor.value);
+        });
+        sidebar.appendChild(fileButton);
+    })
+}
+
+function contentUpdation (content) {
+    if (!currentFolder || !currentFile) return;
+    fileData[currentFolder][currentFile] = content;
+    localStorage.setItem('notes-data', JSON.stringify(fileData));
+}
+
+folderBtn.addEventListener('click', () => {
+    const folderName = prompt('Enter folder name:');
+    if (!folderName || fileData[folderName]) return;
+
+    fileData[folderName] = {};
+    localStorage.setItem('notes-data', JSON.stringify(fileData));
+    renderSidebar();
+})
+
+fileBtn.addEventListener('click', () => {
+    if (!currentFolder) {
+        alert('Select a folder first');
+        return;
+    }
+
+    const fileName = prompt('Enter file name:');
+    if (!fileName || fileData[currentFolder][fileName]) return;
+
+    fileData[currentFolder][fileName] = '';
+    localStorage.setItem('notes-data', JSON.stringify(fileData));
+    renderSidebar();
+})
+
 
 function toggleSidebar () {
     isToggle = !isToggle;
@@ -23,7 +107,7 @@ function toggleSidebar () {
 }
 
 function togglePreview () {
-    isPreveiwVisible = !isPreveiwVisible;
+    isPreviewVisible = !isPreviewVisible;
 
     if (isPreveiwVisible) {
         preview.style.display = 'block';
@@ -47,4 +131,8 @@ document.addEventListener('keydown', (e) => {
 
 sidebarToggleBtn.addEventListener('click', () => {
     toggleSidebar();
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    renderSidebar();
 })
