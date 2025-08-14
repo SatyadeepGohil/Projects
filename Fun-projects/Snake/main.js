3a2403e22a017926abfc9c7bf1e70ef438066f4c
SatyadeepGohil <gohilsatyadeep7@gmail.com>
2025-08-14 17:03:00 +0530

    refactor: removed unused drawGrid and integrate responsive grid logic into Game class.

--- Fun-projects/Snake/main.js
+++ Fun-projects/Snake/main.js
@@ -7,68 +7,38 @@ canvas.height = window.innerHeight;
 const canvasWidth = canvas.width;
 const canvasHeight = canvas.height;
 
-let gridSize = 20;
-function findingGridSize() {
+class Game {
+    constructor () {
+        this.gridSize = this.findingGridSize(20);
+        this.cols = Math.floor(canvasWidth / this.gridSize);
+        this.rows = Math.floor(canvasHeight / this.gridSize);
+        
+        this.totalGridWidth = this.cols * this.gridSize;
+        this.totalGridHeight = this.rows * this.gridSize;
 
-    let currentSize = gridSize;
+        this.offsetX = (canvasWidth - this.totalGridWidth) / 2;
+        this.offsetY = (canvasWidth - this.totalGridHeight) / 2;
 
-    while (currentSize > 10) {
-        const cols = Math.floor(canvasWidth / currentSize);
-        const rows = Math.floor(canvasHeight / currentSize);
-    
-        if (cols % 2 === 0 && rows % 2 === 0) {
-            return currentSize;
+        this.dx,
+        this.dy,
+
+        this.snake;
+    }
+
+    findingGridSize (startSize) {
+        let currentSize = startSize;
+
+        while (currentSize > 10) {
+            const cols = Math.floor(canvasWidth / currentSize);
+            const rows = Math.floor(canvasHeight / currentSize);
+
+            if (cols % 2 === 0 && rows % 2 === 0) {
+                return currentSize;
+            }
+
+            currentSize--;
         }
-    
-        currentSize--;
-    }
 
-    return currentSize;
+        return currentSize;
+    }
 }
-
-gridSize = findingGridSize();
-
-
-function drawGrid () {
-    console.log(gridSize);
-    const cols = Math.floor(canvasWidth / gridSize);
-    const rows = Math.floor(canvasHeight / gridSize);
-
-    const totalGridWidth = cols * gridSize;
-    const totalGridHeight = rows * gridSize;
-
-    const offsetX = (canvasWidth - totalGridWidth) / 2;
-    const offsetY = (canvasHeight - totalGridHeight) / 2;
-
-    console.log('Grid Size:', gridSize);
-    console.log('Canvas Width:', canvasWidth, 'Canvas Height:', canvasHeight);
-    console.log('Total Grid Width', totalGridWidth, 'Total Grid Height', totalGridHeight)
-    console.log('Cols:', cols, 'Rows:', rows);
-    console.log('OffsetX:', offsetX, 'OffsetY:', offsetY);
-
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, canvasWidth, canvasHeight)
-
-
-    ctx.strokeStyle = 'black';
-    ctx.lineWidth = 1;
-
-    for (let i = 0; i <= cols; i++) {
-        const x = offsetX + i * gridSize;
-        ctx.beginPath();
-        ctx.moveTo(x, offsetY);
-        ctx.lineTo(x, offsetY + totalGridHeight);
-        ctx.stroke();
-    }
-
-    for (let j = 0; j <= rows; j++) {
-        const y = offsetY + j * gridSize;
-        ctx.beginPath();
-        ctx.moveTo(offsetX, y);
-        ctx.lineTo(offsetX + totalGridWidth, y);
-        ctx.stroke();
-    }
-
-}
-
-drawGrid();
